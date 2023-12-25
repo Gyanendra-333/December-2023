57afa6ef8061f5daaa699f5591dcd302b3de4433
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-12-25 21:32:31 +0530

    prime number

---
+++ JavaScript/find-prime-number.js
@@ -0,0 +1,8 @@
+let inputValue = 7;
+let isprime = inputValue == 1 ? false : true;  //bcoz 1 is not prime
+
+for (let i = 2; i < inputValue; i++) {
+    inputValue % i == 0 ? isprime *= false : isprime *= true;
+};
+
+console.log(`${inputValue} is ${isprime ? 'prime' : 'not prime'} number`);
