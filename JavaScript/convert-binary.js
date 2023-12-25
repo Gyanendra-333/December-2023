62296f10e954322a8190dc1a7f6ace570e04df9c
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-12-25 21:11:39 +0530

    number to binary

---
+++ JavaScript/convert-binary.js
@@ -0,0 +1,25 @@
+// function dec2bin(dec) {
+//     return (dec >>> 0).toString(2);
+// }
+
+// console.log(dec2bin(1)); // 1
+// console.log(dec2bin(-1)); // 11111111111111111111111111111111
+// console.log(dec2bin(256)); // 100000000
+// console.log(dec2bin(-256)); // 11111111111111111111111100000000
+
+
+// function numTobianary(num) {
+//     return (num >>> 0).toString(2);
+// }
+
+// let convert = numTobianary(3);
+
+// console.log(convert);
+
+
+
+
+function convertNumToBina(num) {
+    return (num >>> 0).toString(2);
+}
+console.log(convertNumToBina(7));
