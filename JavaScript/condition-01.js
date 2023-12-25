dfbb7caa748cdb8ece041a6fc95e41bacd327e0f
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-12-25 22:10:50 +0530

    condition part if else

---
+++ JavaScript/condition-01.js
@@ -0,0 +1,28 @@
+// function candy(parle) {
+//     if (parle == sweet) {
+//         console.log("you can eat");
+//     }
+//     if (parle == sour) {
+//         console.log("you can also eat");
+//     }
+//     else {
+//         console.log("Danger for Kids");
+//     }
+// }
+// candy(parle);
+
+
+
+
+let x = 215;
+let y = 36;
+
+// if (x > y) {
+//     console.log("True");
+// }
+// else {
+//     console.log("False");
+// }
+
+
+x > y ? console.log("true") : console.log("False");
